704b99c0bd0722bbd7628518b186968d7ba3eda0
ayrtonborgesjr <ayrton.borges@yahoo.com.br>
2020-04-16 20:09:48 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public List<Seller> Sellers { get; set; }
+        /*
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            seller.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return 0.0;
+        }
+        */
+    }
+}
